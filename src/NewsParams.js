d4d064e85388c351de41e7295914b4ca94b28227
FlorianGoetz254 <florian.goetz@studierende.htl-donaustadt.at>
2019-06-04 13:13:31 +0200

    NewsParamClass

---
+++ src/NewsParams.js
@@ -0,0 +1,56 @@
+class HeadLineParameter{
+    constructor(){
+
+    }
+    set country(c) {this._country = c}
+    get country() {return this._country}
+    set catagory(c) {this._catagory = c}
+    get catagory() {return this._catagory}
+    set sources(c) {this._sources = c}
+    get sources() {return this._sources}
+    set q(c) {this._q = c}
+    get q() {return this._q}
+    set pageSize(c) {this._pageSize = c}
+    get pageSize() {return this._pageSize}
+    set page(c) {this._page = c}
+    get page() {return this._page}
+    set language(c) {this._language = c}
+    get language() {return this._language}
+}
+
+class EverythingParameter{
+    constructor(){
+
+    }
+    set q(c) {this._q = c}
+    get q() {return this._q}
+    set sources(c) {this._sources = c}
+    get sources() {return this._sources}
+    set domains(c) {this._domains = c}
+    get domains() {return this._domains}
+    set excludeDomains(c) {this._excludeDomains = c}
+    get excludeDomains() {return this._excludeDomains}
+    set from(c) {this._from = c}
+    get from() {return this._from}
+    set to(c) {this._to = c}
+    get to() {return this._to}
+    set language(c) {this._language = c}
+    get language() {return this._language}
+    set sortBy(c) {this._sortBy = c}
+    get sortBy() {return this._sortBy}
+    set pageSize(c) {this._pageSize = c}
+    get pageSize() {return this._pageSize}
+    set page(c) {this._page = c}
+    get page() {return this._page}
+}
+class SourcesParameter{
+    constructor(){
+
+    }
+    set country(c) {this._country = c}
+    get country() {return this._country}
+    set catagory(c) {this._catagory = c}
+    get catagory() {return this._catagory}
+    set language(c) {this._language = c}
+    get language() {return this._language}
+}
