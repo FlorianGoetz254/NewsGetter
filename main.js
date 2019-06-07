8da230afb5877fb22c7aa5b0f03bfc9d01da69f7
FlorianGoetz254 <florian.goetz@studierende.htl-donaustadt.at>
2019-06-07 14:35:17 +0200

    repaired path

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-const ParamClasses = require('./src/NewsParams.js');
-const NewsGetter = require('./src/NewsGetter.js');
-
-const Server = require('src/server.js');
-
-// var p = new ParamClasses.HeadLineParameter();//HeadLineParameter|EverythingParameter|SourcesParameter
-// p.country = 'de'
-
-// NewsGetter.NewsGetter(p, (res) => {
-//     console.log(res)
-// })
+const Server = require('./src/server.js');
